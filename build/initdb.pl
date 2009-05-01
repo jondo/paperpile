11b5f41444c167e2a2def64b8ffd0ca5c2ca7f64
Stefan Washietl <wash@tbi.univie.ac.at>
2009-05-01 19:14:51 +0100

    Added journal list from JabRef and added functions to import it into the application database.

--- build/initdb.pl
+++ build/initdb.pl
@@ -1,9 +1,43 @@
 #!/usr/bin/perl -w
 
+use lib "../catalyst/lib";
+use Paperpile::Model::App;
+
 chdir '../catalyst/db';
+
+print STDERR "Initializing app.db...\n";
 unlink 'app.db';
+my @out = `sqlite3 app.db < app.sql`;
+print @out;
+
+print STDERR "Initializing user.db...\n";
 unlink 'user.db';
-my @out=`sqlite3 app.db < app.sql`;
+@out = `sqlite3 user.db < user.sql`;
 print @out;
-@out=`sqlite3 user.db < user.sql`;
-print @out;
+
+
+print STDERR "Importing journal list into app.db...\n";
+
+open( JOURNALS, "<../data/journals.list" );
+my $model = Paperpile::Model::App->new();
+$model->set_dsn( "dbi:SQLite:" . "../db/app.db" );
+
+$model->dbh->begin_work();
+
+foreach my $line (<JOURNALS>) {
+
+  $line =~ s/;.*$//;
+
+  next if $line =~ /^$/;
+  next if $line =~ /^\s*#/;
+
+  ( my $short, my $long ) = split( /=/, $line );
+
+  $short = $model->dbh->quote($short);
+  $long  = $model->dbh->quote($long);
+
+  $model->dbh->do("INSERT OR IGNORE INTO Journals (short, long) VALUES ($short, $long);");
+}
+
+$model->dbh->commit();
+
--- catalyst/db/app.sql
+++ catalyst/db/app.sql
@@ -2,3 +2,11 @@ CREATE TABLE settings(
  key      TEXT, 
  value    TEXT
 );
+
+CREATE TABLE Journals (
+  short          TEXT UNIQUE,
+  long           TEXT,
+  issn           TEXT,
+  url            TEXT,
+  icon           TEXT
+);
