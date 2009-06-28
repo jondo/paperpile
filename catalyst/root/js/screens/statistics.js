15cceb54eb22a2ee95a60cb15f041256c130060d
Stefan Washietl <wash@tbi.univie.ac.at>
2009-06-28 22:38:16 +0100

    CSS tabs that will be used for charts and tag cloud screens.

--- catalyst/root/js/screens/statistics.js
+++ catalyst/root/js/screens/statistics.js
@@ -4,10 +4,29 @@ Paperpile.Statistics = Ext.extend(Ext.Panel, {
     iconCls: 'pp-icon-statistics',
 
     markup: [
-        '<div class="pp-chart-box pp-box-style1">',
-        '<div id="container">',
+        '<div class="pp-box-tabs">',
+        '<div class="pp-box pp-box-top pp-box-style1" style="height:450px; width:600px; max-width:600px; padding:20px;">',
+        '<div class="pp-container-centered">',
+        '<div id="container" style="display: table-cell;vertical-align: middle;">',
         '<p>INHERE</p>',
         '</div>',
+        '</div>',
+        '</div>',
+
+        '<ul>',
+        '<li class="pp-box-tabs-leftmost pp-box-tabs-active">',
+        '<a href="#" class="pp-textlink pp-bullet" action="top_authors">Top authors</a>',
+        '</li>',
+
+        '<li class="pp-box-tabs-leftmost">',
+        '<a href="#" class="pp-textlink pp-bullet" action="top_journals">Top journals</a>',
+        '</li>',
+        
+        '<li class="pp-box-tabs-leftmost">',
+        '<a href="#" class="pp-textlink pp-bullet" action="pubtypes">Publication types</a>',
+        '</li>',
+        '</ul>',
+
         '</div>'
     ],
 
@@ -27,11 +46,13 @@ Paperpile.Statistics = Ext.extend(Ext.Panel, {
 
         this.tpl.overwrite(this.body, {id:this.id}, true);
 
-        swfobject.embedSWF(
-            Paperpile.Url("/flashchart/open-flash-chart.swf"), "container", "550", "300",
-            "9.0.0", "/expressInstall.swf",
-            {"data-file":"/ajax/charts/test"}
-        );
+        //swfobject.embedSWF(
+        //    Paperpile.Url("/flashchart/open-flash-chart.swf"), "container", "550", "300",
+        //    "9.0.0", "/expressInstall.swf",
+        //    {"data-file":"/ajax/charts/test"}
+        //);
+
+        
 
 
         
