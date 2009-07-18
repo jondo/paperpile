fca85dd316b2dc003ea15592259c852aa0668fe1
Stefan Washietl <wash@paperpile.org>
2009-07-18 18:30:28 +0100

    Update 32bit package.

--- titanium/linux32/start_server.sh
+++ titanium/linux32/start_server.sh
@@ -1,1 +1,6 @@
-../../catalyst/perl5/linux32/bin/perl  ../../catalyst/script/paperpile_server.pl -fork -pidfile ./pid -background 2> /dev/null
+if [ ! -d "$HOME/.paperpile" ]; then
+    mkdir $HOME/.paperpile
+fi
+
+$1./catalyst/perl5/linux32/bin/perl \
+$1./catalyst/script/paperpile_server.pl -fork -pidfile $HOME/.paperpile/server.pid -background 2> /dev/null
