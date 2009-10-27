fafa691ec4df4caad1bc458700f727dd0df6c0c7
Stefan Washietl <wash@tbi.univie.ac.at>
2009-10-26 20:16:28 -0400

    Checking in Andi's new comprehensive journals list.

--- catalyst/db/app.sql
+++ catalyst/db/app.sql
@@ -7,8 +7,13 @@ CREATE TABLE Journals (
   short          TEXT UNIQUE,
   long           TEXT,
   issn           TEXT,
+  essn           TEXT,
+  source         TEXT,
   url            TEXT,
-  icon           TEXT
+  icon           TEXT,
+  reviewed       INTEGER
+
 );
+
 
 CREATE VIRTUAL TABLE Journals_lookup using fts3(long,short);
