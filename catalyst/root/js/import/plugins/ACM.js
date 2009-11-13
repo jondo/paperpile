6fb1e20b498b4d12bf92ffec66fd2c148d8fe38b
Dominic <dominic@domtop.(none)>
2009-11-13 23:41:25 +0100

    all plugin side-panel descriptions are ready

--- catalyst/root/js/import/plugins/ACM.js
+++ catalyst/root/js/import/plugins/ACM.js
@@ -9,6 +9,20 @@ Paperpile.PluginGridACM = Ext.extend(Paperpile.PluginGridOnlineSearch, {
         this.plugin_name = 'ACM';
 
         Paperpile.PluginGridACM.superclass.initComponent.apply(this, arguments);
-    },
+	this.sidePanel = new Paperpile.PluginSidepanelACM();
+    },   
  
+});
+
+Paperpile.PluginSidepanelACM = Ext.extend(Paperpile.PluginSidepanel, {
+
+    markup: [
+        '<div class="pp-box pp-box-side-panel pp-box-style1">',
+        '<div class="pp-acm-logo">&nbsp</div>',
+        '<p class="pp-plugins-description">The ACM Digital Library is the full-text repository of papers from publications that have been published, co-published or co-marketed by the Association for Computing Machinery (ACM). The archive comprises 54000 on-line articles from 30 journals and 900 proceedings.</p>',
+        '<p><a target=_blank href="http://portal.acm.org" class="pp-textlink">portal.acm.org</a></p>',
+        '</div>'],
+
+    tabLabel: 'About ACM',
+   
 });
