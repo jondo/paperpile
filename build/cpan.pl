0d19651307585ddb48a74ee08ca63f02cbea9d7b
Stefan Washietl <wash@mit.edu>
2009-12-20 17:10:21 -0500

    First code to make incremental installation packages.

--- build/cpan.pl
+++ build/cpan.pl
@@ -12,6 +12,8 @@ if ( $arch_string =~ /linux/i ) {
   $platform = ($arch_string =~ /64/) ? 'linux64' : 'linux32';
 }
 
+`unset PERL5LIB`;
+
 # add '.' to @INC to allow access to Bundle::Paperpile.pm
 system ("../catalyst/perl5/$platform/bin/perl -MCPAN -e 'push \@INC, \".\";shell;'");
 
