a687358f45ed6e4b7758622bd4b665eec2b11ba0
Greg <greg@greg-x60.(none)>
2010-01-10 22:04:24 +0000

    Update the Queue schema to include publication sha1s.

--- catalyst/db/queue.sql
+++ catalyst/db/queue.sql
@@ -5,6 +5,7 @@ CREATE TABLE Settings(
 
 CREATE TABLE queue(
   jobid              TEXT UNIQUE,
+  sha1               TEXT,
   status             TEXT,
   error              INTEGER,
   duration           INTEGER
