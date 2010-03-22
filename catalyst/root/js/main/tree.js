f6a328011c9eff3224cbccf5be48496373e1faaa
Stefan Washietl <wash@mit.edu>
2010-03-21 22:12:22 -0400

    Remove debugging code.

--- catalyst/root/js/main/tree.js
+++ catalyst/root/js/main/tree.js
@@ -282,10 +282,9 @@ Ext.extend(Paperpile.Tree, Ext.tree.TreePanel, {
       Paperpile.main.tabs.newScreenTab('Clouds', 'clouds');
       break;
     case 'FEEDBACK':
-      Paperpile.main.addPDFManually({pdf:'/home/wash/testpdf/booth03.pdf', title:'Testtitle', journal:'Nature', volume:'5', authors:'Doe, Joe', year:2000});
-      //if (window.UserVoice) {
-      //  UserVoice.Popin.show()
-      //};
+      if (window.UserVoice) {
+        UserVoice.Popin.show()
+      };
       break;
     case 'DUPLICATES':
       Paperpile.main.tabs.newPluginTab('Duplicates', {},
