403eacb5f31179973d11cccb86edb0455e90ce49
Andreas Gruber <agruber@phibi.(none)>
2010-04-22 00:56:58 +0200

    MetaCrawler update

--- catalyst/lib/Paperpile/MetaCrawler/Targets/Default.pm
+++ catalyst/lib/Paperpile/MetaCrawler/Targets/Default.pm
@@ -19,27 +19,54 @@ package Paperpile::MetaCrawler::Targets::Default;
 use Moose;
 use Paperpile::Formats::HTML;
 use Paperpile::Plugins::Import::PubMed;
+use Paperpile::Utils;
 
 extends 'Paperpile::MetaCrawler::Targets';
 
 sub convert {
 
-  my ($self, $content) = @_;
+  my ( $self, $content ) = @_;
+
+  my $pub;
 
   # Let's try to find some meta tags or
   my $f = new Paperpile::Formats::HTML;
   $f->content($content);
-  my $pub = $f->read();
+  $pub = $f->read();
+
+  # If we are on a site with frames, we might have not
+  # actually parsed what we wanted to. So we search
+  # for frame tags and parse each frame individually.
+  if ( !$pub->title ) {
+
+    # We parse the HTML via TreeBuilder
+    my $tree = HTML::TreeBuilder->new;
+    $tree->utf8_mode(1);
+    $tree->parse_content($content);
+
+    # Get a browser
+    my $browser = Paperpile::Utils->get_browser;
+
+    # let's screen for frame tags and follow the links
+    my @tags = $tree->look_down( '_tag' => 'frame' );
+    foreach my $tag (@tags) {
+      if ( $tag->{src} =~ m/^http/ ) {
+        my $response = $browser->get( $tag->{src} );
+	$f->content($response->content);
+	$pub = $f->read();
+	last if ( $pub->title );
+      }
+    }
+  }
 
   # If we have found a pubmed ID we call the pubmed interface
   if ( $pub->pmid() ) {
-     my $PubMedPlugin = Paperpile::Plugins::Import::PubMed->new();
-    return $PubMedPlugin->_fetch_by_pmid(  $pub->pmid() );
+    my $PubMedPlugin = Paperpile::Plugins::Import::PubMed->new();
+    return $PubMedPlugin->_fetch_by_pmid( $pub->pmid() );
   }
 
   # Once the CrossRef is active, we can parse
   # for a DOI and call it then
-
 
   return $pub;
 }
