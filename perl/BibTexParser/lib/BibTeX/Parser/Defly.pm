de4d7386511846a0398a389c59c079aae07aa306
Andreas Gruber <agruber@phibi.(none)>
2010-05-30 22:43:16 +0200

    Some work on LaTeX to UTF-8 (not completely done yet)

--- perl/BibTexParser/lib/BibTeX/Parser/Defly.pm
+++ perl/BibTexParser/lib/BibTeX/Parser/Defly.pm
@@ -16,12 +16,12 @@ sub uchr {
 }
 
 sub init {
-	my @a = allsrc();
-	0 == @a % 2 or die "odd number of elements in allraw";
-	for my $n (0 .. @a/2 - 1) {
-		my($k, $v) = @a[2 * $n, 1 + 2 * $n];
-		$allfly{$k} = uchr(hex($v));
-	}
+  my @a = allsrc();
+  0 == @a % 2 or die "odd number of elements in allraw";
+  for my $n ( 0 .. @a / 2 - 1 ) {
+    my ( $k, $v ) = @a[ 2 * $n, 1 + 2 * $n ];
+    $allfly{$k} = uchr( hex($v) );
+  }
 }
 
 sub defly_warn {
@@ -43,7 +43,7 @@ our $extma = qr/(?xs)
 		) (?(5)\}|)
 	|
 	\\
-		(?#9)(AA|AE|DH|DJ|L|NG|O|OE|TH|SS|aa|ae|dh|dj|i|j|l|ng|o|oe|ss|th)
+		(?#9)(AA|AE|DH|DJ|L|NG|O|OE|TH|SS|aa|ae|dh|dj|i|j|l|ng|o|oe|ss|th|P|S)
 		(?![a-zA-Z])[ \t]*\s?
 	|
 	\\uchar (?![a-zA-Z]) (?: \{ [ \t]*\s?(?:
@@ -56,39 +56,40 @@ our $extma = qr/(?xs)
 /;
 
 sub extva {
-	my $all = $1;
-	my $trf = $3 || $4;
-	my $bas = $6 || $7;
-	my $seu = $9;
-	my $cod = defined($11) ? hex($11) : defined($12) ? oct($12) : $10;
-	my $baserr = defined($8);
-	my $coderr = defined($13);
-	$defly_debug and warn "DEBUG defly: ext match: " . do {
-		no warnings "uninitialized"; "all ($all) trf ($trf) bas ($bas) seu ($seu) cod ($cod) baserr ($baserr) coderr ($coderr)"; 
-	};
-	my $k;
-	#print "$all $trf $bas $seu $cod $baserr $coderr\n";
-	if ($baserr) {
-		defly_warn "unsupported flying accent format ($all)";
-	} elsif ($coderr) {
-		defly_warn "unsupported use of \\uchar ($all)";
-	} elsif ($trf) {
-		$k = "\\" . $trf . "{" . $bas . "}";
-	} elsif ($seu) {
-		$k = "\\" . $seu;
-	} elsif ($cod) {
-		return uchr($cod);
-	} else {
-		defly_warn "bug in flying accent handling code";
-	}
-	if (defined($k)) {
-		if (defined(my $v = $allfly{$k})) {
-			return $v;
-		} else {
-			defly_warn "unknown flying accented letter ($all)";
-		}
-	}
-	return $all;
+  my $all    = $1;
+  my $trf    = $3 || $4;
+  my $bas    = $6 || $7;
+  my $seu    = $9;
+  my $cod    = defined($11) ? hex($11) : defined($12) ? oct($12) : $10;
+  my $baserr = defined($8);
+  my $coderr = defined($13);
+  $defly_debug and warn "DEBUG defly: ext match: " . do {
+    no warnings "uninitialized";
+    "all ($all) trf ($trf) bas ($bas) seu ($seu) cod ($cod) baserr ($baserr) coderr ($coderr)";
+  };
+  my $k;
+
+  if ($baserr) {
+    defly_warn "unsupported flying accent format ($all)";
+  } elsif ($coderr) {
+    defly_warn "unsupported use of \\uchar ($all)";
+  } elsif ($trf) {
+    $k = "\\" . $trf . "{" . $bas . "}";
+  } elsif ($seu) {
+    $k = "\\" . $seu;
+  } elsif ($cod) {
+    return uchr($cod);
+  } else {
+    defly_warn "bug in flying accent handling code";
+  }
+  if ( defined($k) ) {
+    if ( defined( my $v = $allfly{$k} ) ) {
+      return $v;
+    } else {
+      defly_warn "unknown flying accented letter ($all)";
+    }
+  }
+  return $all;
 }
 
 sub defly {
@@ -292,6 +293,8 @@ sub allsrc {
     \.{z} 17c
     \v{Z} 17d
     \v{z} 17e
+    \P b6
+    \S a7
   );
 }
 
