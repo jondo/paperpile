4152b7bc9884e9a9316523718b339abc7780a96e
greg <greg@ubuntu.ubuntu-domain>
2010-08-11 05:58:41 +0100

    Revert PDF viewer Ajax calls to Ext.Ajax.request; they dont work with teh paperpile.Ajax abstraction because they parse XML rather than JSON data.

--- catalyst/root/js/pdf/viewer.js
+++ catalyst/root/js/pdf/viewer.js
@@ -97,13 +97,6 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
     var transparentDot = new Image(1, 1);
     transparentDot.src = this.getOnePixel();
 
-    // Handle initial options.
-    //    this.columnCount = this.columns;
-    //    if (this.pageLayout == "continuous")
-    //      this.continuous = true;
-    //    else
-    //      this.continuous = false;
-    // GJ 2009-10-20 override defaults.
     this.columnCount = 1;
     this.continuous = true;
     if (this.zoom == "page") this.specialZoom = 'page';
@@ -423,6 +416,8 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
     var pagesId = this.prefix() + "pages";
     var contentId = this.prefix() + "content";
 
+      Paperpile.log("debug 2");
+      
     Ext.apply(this, {
       autoScroll: true,
       enableKeyEvents: true,
@@ -435,13 +430,16 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
 
     this.on('render', this.myAfterRender, this);
     this.on('resize', this.myOnResize, this);
+
+    Paperpile.log("debug 3");
+
   },
 
   myAfterRender: function() {
     if (this.file != "") {
       this.initPDF(this.file);
     }
-
+      Paperpile.log("debug 4");
     this.focusEl = this.el.createChild({
       tag: 'a',
       cls: 'x-menu-focus',
@@ -481,6 +479,7 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
       this.slidePreview();
     },
     this);
+Paperpile.log("debug 5");
   },
 
   getOnePixel: function() {
@@ -556,7 +555,7 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
     this.file = file,
 
     //    this.log("Init!");
-    Paperpile.Ajax({
+    Ext.Ajax.request({
       url: '/ajax/pdf/extpdf',
       params: {
         command: 'INFO',
@@ -1277,7 +1276,7 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
     this.lastSearchText = searchText;
     this.fly("search_field").addClass("pdf-search-busy");
 
-    Paperpile.Ajax({
+    Ext.Ajax.request({
       url: '/ajax/pdf/extpdf',
       params: {
         command: 'SEARCH',
@@ -1641,7 +1640,7 @@ Paperpile.PDFviewer = Ext.extend(Ext.Panel, {
     if (this.words[pageIndex].length > 0) return;
 
     //this.log("Loading words...");
-    Paperpile.Ajax({
+    Ext.ajax.Request({
       url: '/ajax/pdf/extpdf',
       params: {
         command: 'WORDLIST',
