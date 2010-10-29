bfe7fb483e89d514a6d44a85ce7209d54404ad54
Stefan Washietl <stefan@paperpile.com>
2010-10-28 21:22:12 -0400

    Update licensing and README files.

--- build/hudson/default.pl
+++ build/hudson/default.pl
@@ -5,16 +5,17 @@
 # This file is part of Paperpile
 #
 # Paperpile is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
 # Paperpile is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.  You should have received a
-# copy of the GNU General Public License along with Paperpile.  If
-# not, see http://www.gnu.org/licenses.
+# Affero General Public License for more details.  You should have
+# received a copy of the GNU Affero General Public License along with
+# Paperpile.  If not, see http://www.gnu.org/licenses.
+
 
 # Our build machine is 32 bit and Hudson runs this script from the top
 # level in the workspace. So we need the above perl binary.
--- catalyst/lib/Paperpile/Controller/Ajax/Files.pm
+++ catalyst/lib/Paperpile/Controller/Ajax/Files.pm
@@ -3,16 +3,17 @@
 # This file is part of Paperpile
 #
 # Paperpile is free software: you can redistribute it and/or modify it
-# under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 
 # Paperpile is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# General Public License for more details.  You should have received a
-# copy of the GNU General Public License along with Paperpile.  If
-# not, see http://www.gnu.org/licenses.
+# Affero General Public License for more details.  You should have
+# received a copy of the GNU Affero General Public License along with
+# Paperpile.  If not, see http://www.gnu.org/licenses.
+
 
 package Paperpile::Controller::Ajax::Files;
 
