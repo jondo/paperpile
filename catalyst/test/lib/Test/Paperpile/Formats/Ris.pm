048251ccab647d19ebe5f9fc1027e8acdba54db5
Dominic Rose <dominic@bipc30.informatik.uni-freiburg.de>
2011-03-11 13:48:14 +0100

    Ris test for Nature

--- catalyst/test/lib/Test/Paperpile/Formats/Ris.pm
+++ catalyst/test/lib/Test/Paperpile/Formats/Ris.pm
@@ -19,7 +19,7 @@ sub startup : Tests(startup => 1) {
 
 # Add test functions here
 
-sub read : Tests(48) {
+sub read : Tests(62) {
 
   my ($self) = @_;
 
@@ -43,7 +43,7 @@ sub read : Tests(48) {
 }
 
 
-sub write : Tests(4) {
+sub write : Tests(5) {
 
   my ($self) = @_;
 
