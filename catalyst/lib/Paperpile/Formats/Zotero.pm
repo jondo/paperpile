ff12c460edaeffa38a1c745f506572edd26a4f33
Andreas Gruber <agruber@tbi.univie.ac.at>
2011-03-13 14:21:12 +0100

    Update in Zotero module

--- catalyst/lib/Paperpile/Formats/Zotero.pm
+++ catalyst/lib/Paperpile/Formats/Zotero.pm
@@ -83,11 +83,11 @@ sub read {
     #34 document        35 encyclopediaArticle 36 dictionaryEntry
 
     my (
-      $title,  $authors, $journal, $issue,        $volume,    $year,
-      $month,  $ISSN,    $pages,   $doi,          $abstract,  $booktitle,
-      $url,    $pmid,    $arxivid, $editors,      $publisher, $edition,
-      $series, $address, $school,  $howpublished, $note,      $isbn,
-      $labels
+      $title,    $authors,      $journal, $issue,  $volume,
+      $year,     $month,        $ISSN,    $pages,  $doi,
+      $abstract, $booktitle,    $url,     $pmid,   $arxivid,
+      $editors,  $publisher,    $edition, $series, $address,
+      $school,   $howpublished, $note,    $isbn,   $labels
     );
 
     my @pdfs                 = ();
@@ -142,35 +142,38 @@ sub read {
       #122 genre               123 archive
 
       my %unsupported = (
-        '2'   => 'rights',
+        '2' => 'rights',
+
         #'14'  => 'date',
-        '15'  => 'section',
-        '18'  => 'callNumber',
-        '19'  => 'archiveLocation',
-        '21'  => 'distributor',
+        '15' => 'section',
+        '18' => 'callNumber',
+        '19' => 'archiveLocation',
+        '21' => 'distributor',
+
         #'22'  => 'extra',
         #'27'  => 'accessDate',
-        '29'  => 'seriesText',
-        '30'  => 'seriesNumber',
-        '32'  => 'reportType',
-        '36'  => 'code',
-        '40'  => 'session',
-        '41'  => 'legislativeBody',
-        '42'  => 'history',
-        '43'  => 'reporter',
-        '44'  => 'court',
-        '45'  => 'numberOfVolumes',
-        '46'  => 'committee',
-        '48'  => 'assignee',
-        '50'  => 'patentNumber',
-        '51'  => 'priorityNumbers',
-        '52'  => 'issueDate',
-        '53'  => 'references',
-        '54'  => 'legalStatus',
-        '55'  => 'codeNumber',
-        '59'  => 'artworkMedium',
-        '60'  => 'number',
-        '61'  => 'artworkSize',
+        '29' => 'seriesText',
+        '30' => 'seriesNumber',
+        '32' => 'reportType',
+        '36' => 'code',
+        '40' => 'session',
+        '41' => 'legislativeBody',
+        '42' => 'history',
+        '43' => 'reporter',
+        '44' => 'court',
+        '45' => 'numberOfVolumes',
+        '46' => 'committee',
+        '48' => 'assignee',
+        '50' => 'patentNumber',
+        '51' => 'priorityNumbers',
+        '52' => 'issueDate',
+        '53' => 'references',
+        '54' => 'legalStatus',
+        '55' => 'codeNumber',
+        '59' => 'artworkMedium',
+        '60' => 'number',
+        '61' => 'artworkSize',
+
         #'62'  => 'libraryCatalog',
         '63'  => 'videoRecordingFormat',
         '64'  => 'interviewMedium',
@@ -215,6 +218,7 @@ sub read {
         '112' => 'nameOfAct',
         '113' => 'subject',
         '114' => 'proceedingsTitle',
+
         #'116' => 'shortTitle',
         '117' => 'docketNumber',
         '118' => 'numPages',
@@ -263,7 +267,7 @@ sub read {
       if ( $fieldID == 12 ) {
         $journal = $dbh->selectrow_array( $statement, undef, $valueID );
         if ( $journal =~ m/\d+\.\d+/ ) {
-          $arxivid = "arXiv:$journal";
+          $arxivid = "$journal";
           $journal = "Preprint";
         }
       }
@@ -277,9 +281,11 @@ sub read {
       $ISSN = $dbh->selectrow_array( $statement, undef, $valueID )
         if ( $fieldID == 13 );
       $isbn = $dbh->selectrow_array( $statement, undef, $valueID )
-	if ( $fieldID == 11 );
-      $doi = $dbh->selectrow_array( $statement, undef, $valueID )
-        if ( $fieldID == 26 );
+        if ( $fieldID == 11 );
+      if ( $fieldID == 26 ) {
+        $doi = $dbh->selectrow_array( $statement, undef, $valueID );
+        $doi =~ s/(doi:?\s*)(10\.\d{4}.*)/$2/i;
+      }
       $abstract = $dbh->selectrow_array( $statement, undef, $valueID )
         if ( $fieldID == 90 );
       $booktitle = $dbh->selectrow_array( $statement, undef, $valueID )
@@ -304,8 +310,8 @@ sub read {
 
       # now we add everything else to notes that has not been parsed yet
       if ( defined $unsupported{$fieldID} ) {
-	my $tmpstring = $dbh->selectrow_array( $statement, undef, $valueID );
-	push @notes_dummy, "ZOTERO field \"$unsupported{$fieldID}\": $tmpstring";
+        my $tmpstring = $dbh->selectrow_array( $statement, undef, $valueID );
+        push @notes_dummy, "ZOTERO field \"$unsupported{$fieldID}\": $tmpstring";
       }
 
     }
@@ -316,8 +322,7 @@ sub read {
     $sth3->execute($itemID);
     my @tmpauthors = ();
     my @tmpeditors = ();
-    my $statement =
-      'SELECT lastName || ", " || firstName ' . 'FROM creatorData ' . 'WHERE creatorDataID=?;';
+    my $statement  = 'SELECT * FROM creatorData WHERE creatorDataID=?;';
     while ( my @tmp3 = $sth3->fetchrow_array ) {
       ( my $itemID, my $creatorID, my $creatorTypeID, my $orderIndex ) = @tmp3;
 
@@ -332,10 +337,37 @@ sub read {
       # 22 artist       23 commenter        24 presenter
       # 25 guest        26 podcaster        27 reviewedAuthor
       # 28 cosponsor    29 bookAuthor
-      if ( $creatorTypeID == 3 or $creatorTypeID == 5 ) {
-        push @tmpeditors, $dbh->selectrow_array( $statement, undef, $creatorID );
+
+      ( my $cratorDataID, my $firstName, my $lastName, my $shortName, my $fieldMode ) =
+        $dbh->selectrow_array( $statement, undef, $creatorID );
+
+
+      my $tmp_name = '';
+
+      if ( defined $fieldMode ) {
+	if ( $fieldMode == 1 ) {
+	  $tmp_name = "{$lastName}" if ( defined $lastName and not defined $firstName);
+	} else {
+	  if ( defined $lastName and defined $firstName) {
+	    $tmp_name = "$lastName, $firstName";
+	  } elsif ( defined $lastName and not defined $firstName) {
+	    $tmp_name = $lastName;
+	  }
+	}
       } else {
-        push @tmpauthors, $dbh->selectrow_array( $statement, undef, $creatorID );
+	if ( defined $lastName and defined $firstName) {
+	  $tmp_name = "$lastName, $firstName";
+	} elsif ( defined $lastName and not defined $firstName) {
+	  $tmp_name = $lastName;
+	}
+      }
+
+      next if ( $tmp_name eq '' );
+
+      if ( $creatorTypeID == 3 or $creatorTypeID == 5 ) {
+        push @tmpeditors, $tmp_name;
+      } else {
+        push @tmpauthors, $tmp_name;
       }
     }
     $authors = join( " and ", @tmpauthors );
@@ -388,8 +420,9 @@ sub read {
     }
 
     # get labels as comma separated list
-    my $sth7 = $dbh->prepare('SELECT name FROM itemTags JOIN tags ON itemTags.tagID = tags.tagID WHERE itemID = ?;');
-    $sth7->execute( $itemID );
+    my $sth7 = $dbh->prepare(
+      'SELECT name FROM itemTags JOIN tags ON itemTags.tagID = tags.tagID WHERE itemID = ?;');
+    $sth7->execute($itemID);
     my @labels_tmp = ();
     while ( my @t = $sth7->fetchrow_array ) {
       push @labels_tmp, $t[0];
@@ -438,8 +471,9 @@ sub read {
     }
 
     # add unsupported fields to note tag
-    $note .= "<br />\n".join("<br />\n", @notes_dummy)."<br />\n" if ( $#notes_dummy  > -1 and $note);
-    $note = join("<br />\n", @notes_dummy)."<br />\n" if ( $#notes_dummy  > -1 and !$note);
+    $note .= "<br />\n" . join( "<br />\n", @notes_dummy ) . "<br />\n"
+      if ( $#notes_dummy > -1 and $note );
+    $note = join( "<br />\n", @notes_dummy ) . "<br />\n" if ( $#notes_dummy > -1 and !$note );
 
     my $pub = Paperpile::Library::Publication->new( pubtype => $pubtype );
 
@@ -465,12 +499,13 @@ sub read {
     $pub->howpublished($howpublished) if $howpublished;
     $pub->school($school)             if $school;
     $pub->annote($note)               if $note;
-    $pub->labels_tmp($labels)                 if $labels;
+    $pub->linkout($url)               if $url;
+    $pub->labels_tmp($labels)         if $labels;
 
     # add PDFs and other attachements
     foreach my $i ( 0 .. $#pdfs ) {
       if ( $i == 0 ) {
-        $pub->_pdf_tmp($pdfs[$i]);
+        $pub->_pdf_tmp( $pdfs[$i] );
       } else {
         unshift( @regular_attachements, $pdfs[$i] );
       }
