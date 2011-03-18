1ed11e8921048c013150ae608aed98c2a821833c
Greg Jordan <gjuggler@gmail.com>
2011-03-18 19:38:40 +0000

    Remove some stuff from import / grid folder.

--- catalyst/root/js/import/toolbar.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Paperpile.Toolbar', {
-	extend: 'Ext.toolbar.Toolbar',
-  menuBreakItemId: 'asdf',
-  initComponent: function() {
-    Ext.apply(this, {
-    });
-    Paperpile.Toolbar.superclass.initComponent.call(this);
-  }
-});
